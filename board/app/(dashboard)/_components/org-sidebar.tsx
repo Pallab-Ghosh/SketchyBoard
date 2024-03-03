d10306abd9077c01871dda847ae82b6d131608be
PALLAB GHOSH <gpallab405@gmail.com>
2024-03-03 19:30:37 +0530

    Create Dahboard which will visible in large device

---
+++ board/app/(dashboard)/_components/org-sidebar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const OrganizationSidebar = () => {
+  return (
+    <div className=' hidden bg-sky-500 lg:flex space-y-6 w-[206px] pl-5 pt-5 text-white'>
+           organization dashboard
+    </div>
+  )
+}
+
+export default OrganizationSidebar
