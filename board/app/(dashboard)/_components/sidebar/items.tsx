ec5d9266614a50c61f4e169b39faafbc52c17584
PALLAB GHOSH <gpallab405@gmail.com>
2024-03-08 19:29:56 +0530

    Update the component , fetch the organizations , get active organization and create button if click on item that item will be set as as current organization

--- board/app/(dashboard)/_components/sidebar/items.tsx
+++ board/app/(dashboard)/_components/sidebar/items.tsx
@@ -1,10 +1,47 @@
 'use client'
 
-import React from 'react'
+import { cn } from "@/lib/utils"
+import { useOrganization, useOrganizationList } from "@clerk/nextjs"
+import Image from "next/image"
 
-const Items = () => {
+type ItemsProps={
+  id:string,
+  name:string,
+  imageUrl:string
+}
+
+
+
+
+const Items = ({id,name,imageUrl}:ItemsProps) => {
+  
+  const {organization}=useOrganization();
+  const {setActive}=useOrganizationList();
+ 
+  const isActive=organization?.id === id;
+
+
+  const onClick=()=>{
+
+     if(!setActive)
+       return;
+
+     setActive({organization : id});
+  }
+  
   return (
-    <div>items</div>
+    <div className=" aspect-square relative">
+          <Image
+             fill
+             alt={name}
+             src={imageUrl}
+             onClick={onClick}
+             className={cn(
+              "rounded-md cursor-pointer opacity-45 transition",
+               isActive  &&  'opacity-100'
+             )}
+          />
+    </div>
   )
 }
 
