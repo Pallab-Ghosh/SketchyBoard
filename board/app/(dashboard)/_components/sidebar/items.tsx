9ed80ddace65e724ca60d34abae832962954f961
PALLAB GHOSH <gpallab405@gmail.com>
2024-03-08 20:10:27 +0530

    Wrap the button entirely with Hint component sent the necessary props

--- board/app/(dashboard)/_components/sidebar/items.tsx
+++ board/app/(dashboard)/_components/sidebar/items.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { Hint } from "@/components/hint"
 import { cn } from "@/lib/utils"
 import { useOrganization, useOrganizationList } from "@clerk/nextjs"
 import Image from "next/image"
@@ -31,16 +32,18 @@ const Items = ({id,name,imageUrl}:ItemsProps) => {
   
   return (
     <div className=" aspect-square relative">
-          <Image
-             fill
-             alt={name}
-             src={imageUrl}
-             onClick={onClick}
-             className={cn(
-              "rounded-md cursor-pointer opacity-45 transition",
-               isActive  &&  'opacity-100'
-             )}
-          />
+        <Hint  label={name} side='right' align='start' sideOffset={18} >
+            <Image
+                fill
+                alt={name}
+                src={imageUrl}
+                onClick={onClick}
+                className={cn(
+                  "rounded-md cursor-pointer opacity-55 hover:opacity-100 transition",
+                  isActive  &&  'opacity-100'
+                )}
+              />
+        </Hint>
     </div>
   )
 }
