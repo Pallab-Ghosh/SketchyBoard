c56720db15b394378bda747dadfde2993c3ce20a
PALLAB GHOSH <gpallab405@gmail.com>
2024-03-08 20:09:52 +0530

    Wrap the button entirely with Hint component sent the necessary props

--- board/app/(dashboard)/_components/sidebar/new-button.tsx
+++ board/app/(dashboard)/_components/sidebar/new-button.tsx
@@ -1,3 +1,4 @@
+import { Hint } from '@/components/hint'
 import { Button } from '@/components/ui/button'
 import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog'
 import { CreateOrganization } from '@clerk/nextjs'
@@ -12,9 +13,16 @@ const  NewButton = () => {
           <Dialog>
                <DialogTrigger asChild>
                    <div className='aspect-square'>
-                        <button className=' bg-white/25 h-full w-full rounded-md flex items-center justify-center opacity-60 hover:opacity-100 transition'>
-                            <Plus className=' text-white'/>
-                        </button>
+                       <Hint 
+                       label='Create Organization'
+                       side='right'
+                       align='start'
+                       sideOffset={18}
+                       >
+                              <button className=' bg-white/25 h-full  w-full rounded-md flex items-center justify-center opacity-60 hover:opacity-100 transition'>
+                                    <Plus className=' text-white'/>
+                              </button>
+                       </Hint>
                    </div>
                </DialogTrigger> 
 
