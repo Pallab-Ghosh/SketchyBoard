cb9959a73bd625ad82dee52251269277d3d6054f
PALLAB GHOSH <gpallab405@gmail.com>
2024-03-09 23:54:43 +0530

    Render the Organization name imageUrl to <Items/> component

--- board/app/(dashboard)/_components/sidebar/list.tsx
+++ board/app/(dashboard)/_components/sidebar/list.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useOrganizationList } from "@clerk/nextjs"
+import Items from "./items"
 
  
 
@@ -12,6 +13,8 @@ const List = () => {
         }
     })
 
+    console.log(userMemberships)
+
     if(!userMemberships.data?.length)return null;
 
 
@@ -20,9 +23,13 @@ const List = () => {
     <ul className=" space-y-4">
         {
             userMemberships.data?.map((member)=>(
-               <p key={member.organization.id}>
-                   {member.organization.name}
-               </p>
+                <Items
+                  key={member.organization.id}
+                  id={member.organization.id}
+                  name={member.organization.name}
+                  imageUrl={member.organization.imageUrl}
+                />
+              
             ))
         }
     </ul>
