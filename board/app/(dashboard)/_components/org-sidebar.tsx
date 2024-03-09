bca2bf0ab1901c0732d7edf82063590e1cda95dd
PALLAB GHOSH <gpallab405@gmail.com>
2024-03-10 00:33:35 +0530

    Change the styling of  <OrganizationSwitcher/> elments and organizationSwitcherTrigger, also add the button of team boards and Favorite Boards , set the query true and give pathname under a object in Link

--- board/app/(dashboard)/_components/org-sidebar.tsx
+++ board/app/(dashboard)/_components/org-sidebar.tsx
@@ -1,7 +1,9 @@
 'use client'
 
+import { Button } from "@/components/ui/button"
 import { cn } from "@/lib/utils"
 import { OrganizationSwitcher } from "@clerk/nextjs"
+import { LayoutDashboard, Star } from "lucide-react"
 import { Poppins } from "next/font/google"
 import Image from "next/image"
 import Link from "next/link"
@@ -27,8 +29,58 @@ const OrganizationSidebar = () => {
          
          <OrganizationSwitcher
           hidePersonal
-          
+          appearance={
+            {
+            elements:{
+                 rootBox:{
+                  display:'flex',
+                  justifyContent:'center',
+                  alignItems:'center',
+                  width:'100%'
+                },
+                 organizationSwitcherTrigger:{
+                  padding:'6px',
+                  width:'100%',
+                  borderRadius:'8px',
+                  border:'1px solid #E5E7EB',
+                  backgroundColor:'white'
+
+                 }
+            }
+            }
+        }
          />
+
+         <div className=" space-y-1 w-full">
+                   <Button
+                    asChild
+                    variant='ghost'
+                    size='lg'
+                    className=" font-normal justify-start px-2 w-full"
+                   >
+                      <Link href='/'>
+                            <LayoutDashboard className=" h-4 w-4 mr-2"/>
+                            Team Boards
+                      </Link>
+                   </Button>
+
+                   <Button
+                    asChild 
+                    variant='ghost'
+                    size='lg'
+                    className=" font-normal justify-start px-2 w-full"
+                   >
+                      <Link href={
+                        {
+                          pathname:'/',
+                          query:{favorites : true}
+                        }
+                      }>
+                            <Star className=" h-4 w-4 mr-2"/>
+                            Favorites Boards
+                      </Link>
+                   </Button>
+         </div>
     </div>
   )
 }
