f8bfa0a79d0d898d03fc8b0593a69c09b66078b9
PALLAB GHOSH <gpallab405@gmail.com>
2024-03-09 23:53:07 +0530

    Update component create font , Link with Image , and Integrate <OrganizationSwitcher/> from Clerk

--- board/app/(dashboard)/_components/org-sidebar.tsx
+++ board/app/(dashboard)/_components/org-sidebar.tsx
@@ -1,11 +1,36 @@
-import React from 'react'
+'use client'
+
+import { cn } from "@/lib/utils"
+import { OrganizationSwitcher } from "@clerk/nextjs"
+import { Poppins } from "next/font/google"
+import Image from "next/image"
+import Link from "next/link"
+
+const font = Poppins({
+  subsets : ['latin'],
+  weight:['600']
+})
+
+
 
 const OrganizationSidebar = () => {
   return (
-    <div className=' hidden bg-sky-500 lg:flex space-y-6 w-[206px] pl-5 pt-5 text-white'>
-           organization dashboard
+    <div className=' hidden  lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5  text-black '>
+          <Link href='/'>
+               <div className=" flex items-center gap-x-2 hover:underline">
+                      <Image  src="/logo.svg"  alt="logo"  height={60}  width={60} /> 
+                      <span className={cn(  "font-semibold text-2xl",font.className)}>
+                        Board
+                      </span>
+               </div>
+          </Link>
+         
+         <OrganizationSwitcher
+          hidePersonal
+          
+         />
     </div>
   )
 }
-
+ 
 export default OrganizationSidebar
