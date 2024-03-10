cb172100520ccdbd984cb230bf7a7dd87cda0d10
PALLAB GHOSH <gpallab405@gmail.com>
2024-03-10 20:11:32 +0530

    Update component
    1.Add states , get the value using debounce hook with a specific time and modify the url object .
    
    2. Create a function that get the value from input elment

--- board/app/(dashboard)/_components/search-input.tsx
+++ board/app/(dashboard)/_components/search-input.tsx
@@ -1,22 +1,55 @@
 'use client'
 
 import { Input } from "@/components/ui/input"
+ 
+ 
 import { Search } from "lucide-react"
-
-
+import { useRouter } from "next/navigation"
+import qs from 'query-string';
+import { ChangeEvent, useEffect, useState } from "react"
+import {useDebounceValue } from 'usehooks-ts'
 
 
 
 const SearchInput = () => {
 
+const router=useRouter()
+const [value , setvalues]=useState("");
+const [debounceValue , setvalue]= useDebounceValue (value,2000)
+
+
+const handleChange=(e: ChangeEvent<HTMLInputElement>)=>{
+    setvalues(e.target.value)
+}
+
+
+useEffect(()=>{
+    const url=qs.stringifyUrl({
+        url:'/',
+        query :{
+            search : debounceValue
+        }
+    } , {skipEmptyString : true , skipNull : true})
+
+    router.push(url)
+} , [debounceValue , router])
+
+
 
   return (
     <div className=" w-full relative">
+
        <Search
-       className=" absolute top-1/2 left-3 transform -translate-y-1/2 text-muted-foreground"
+       className=" absolute top-1/2 left-3 transform -translate-y-1/2 
+       text-muted-foreground h-4 w-4"
        />
+
        <Input
         className=" w-full max-w-[510px] pl-9"
+        placeholder="Search Boards"
+        onChange={handleChange}
+        value={value}
+        
        />
     </div>
   )
