e60e9ad7c4fd6ca8b090f7fa336a1af288f4c3eb
PALLAB GHOSH <gpallab405@gmail.com>
2024-03-15 13:51:49 +0530

    Add a condition that if no active organization then render the component <EmptyOrg/> else show Boar List

--- board/app/(dashboard)/page.tsx
+++ board/app/(dashboard)/page.tsx
@@ -1,11 +1,19 @@
-
+'use client'
 
 import React from 'react'
+import EmptyOrg from './_components/empty-org'
+import { useOrganization } from '@clerk/nextjs';
 
 const Dashboardpage = () => {
+
+  const {organization}=useOrganization();
+
+
   return (
-    <div className=' bg-slate-300 flex-1 h-[calc(100%-80px)] p-6'>
-       Dashboard page
+    <div className=' bg-slate-100 flex-1 h-[calc(100%-80px)] p-6'>
+      {
+        !organization ?   <EmptyOrg/> : <p>Board List</p>
+      }
     </div>
   )
 }
