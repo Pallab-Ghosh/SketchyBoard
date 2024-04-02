ff91b242a2825c0a4748e2428d8b4d30cf6efbaa
PALLAB GHOSH <gpallab405@gmail.com>
2024-04-02 17:04:33 +0530

    Add these board in boardcard

--- board/app/(dashboard)/_components/board-card/index.tsx
+++ board/app/(dashboard)/_components/board-card/index.tsx
@@ -6,6 +6,8 @@ import {formatDistanceToNow} from 'date-fns'
 import { useAuth } from '@clerk/nextjs'
 import Footer from './footer'
 import { Skeleton } from "@/components/ui/skeleton"
+import Action from '@/components/action'
+import { MoreHorizontal } from 'lucide-react'
 
 
 type BoardCardProps={
@@ -38,9 +40,18 @@ const BoardCard = ({key,id , title, imageUrl, authorID , authorName , createdAt 
                             src={imageUrl}
                             alt='doodle'
                             fill
-                            className=' object-fit'
+                            className=' object-fit hover:bg-stone-400'
                         />
-                   <Overlay/>  
+ 
+                     
+                   <Action id={id} title={title}  side="right">
+                      <button className=' absolute top-1 right-1 opacity-0 group-hover:opacity-100 transition-opacity px-3 py-2 outline-none'>
+                          <MoreHorizontal
+                          className='opacity-75 text-zinc-50 hover:opacity-100 transition-opacity'
+                          />
+                      </button>
+                   </Action>
+
                 </div> 
 
                 <Footer
