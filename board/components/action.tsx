c6c5b23f5f8c58e1f6d7ffbb83b746dd448db6a5
PALLAB GHOSH <gpallab405@gmail.com>
2024-04-02 19:17:01 +0530

    Create a function that it will create a link using window.location.origin using id and save it on cliipboard and invoke this function with dropdownmenuitem

--- board/components/action.tsx
+++ board/components/action.tsx
@@ -3,6 +3,7 @@
 import { DropdownMenuContentProps, DropdownMenuItem} from '@radix-ui/react-dropdown-menu'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from './ui/dropdown-menu'
 import { Link2 } from 'lucide-react'
+import { toast } from 'sonner'
 
 type ActionProps = {
    children :React.ReactNode,
@@ -14,17 +15,32 @@ type ActionProps = {
 
 
 const Action = ({children , side , sideOffset , id , title}:ActionProps) => {
+  
+  const onCopyLink = ()=>{
+    navigator.clipboard.writeText(
+      `${window.location.origin}/board/${id}`
+    )
+    .then(()=>{toast.success('Link copied') })
+     .catch(()=>{toast.error('Failed to copy Link')})
+  }
+  
+  
   return (
+ 
       <DropdownMenu>
+
           <DropdownMenuTrigger asChild>
              {children}
           </DropdownMenuTrigger>
+
           <DropdownMenuContent side={side} 
             sideOffset={sideOffset} 
              onClick={(e)=>e.stopPropagation()}
-             className='w-60'>
+             className='w-60'
+             
+             >
               
-                <DropdownMenuItem className=' p-3 cursor-pointer flex'>
+                <DropdownMenuItem className=' p-3 cursor-pointer flex' onClick={onCopyLink}>
                      <Link2 className='h-4 w-4 mr-2'/>
                      Copy Board Link
                   </DropdownMenuItem>
