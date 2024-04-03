06888858e4db320ba3b3cce0115cda109d3a2d00
PALLAB GHOSH <gpallab405@gmail.com>
2024-04-03 19:40:24 +0530

    Add     <ModelProviders/> for active this component's UI

--- board/app/layout.tsx
+++ board/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ConvexClientProvider } from "@/providers/convex-client-provider";
 import { Toaster } from "@/components/ui/sonner";
+import ModelProviders from "@/providers/model-providers";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body className={inter.className}>
          <ConvexClientProvider>
               <Toaster/>
+              <ModelProviders/>
              {children}
          </ConvexClientProvider>
         </body>
