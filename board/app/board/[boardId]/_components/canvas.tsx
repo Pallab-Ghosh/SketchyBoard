5a0d07dfe76124588bd5ea2351c5dd0e87c0390d
PALLAB GHOSH <gpallab405@gmail.com>
2024-04-09 16:25:22 +0530

    Create Canvas component on page.tsx

---
+++ board/app/board/[boardId]/_components/canvas.tsx
@@ -0,0 +1,11 @@
+ 'use client'
+
+const Canvas = () => {
+  return (
+    <main className="h-full w-full bg-neutral-100 relative touch-none">
+        canvas
+        </main>
+  )
+}
+
+export default Canvas
