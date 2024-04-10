f373dd9ba2af257716b9c35b6987b1af95c6b7ac
PALLAB GHOSH <gpallab405@gmail.com>
2024-04-10 18:58:03 +0530

    Craete this  wrapper component get the id and children as a prop and render with suspense

---
+++ board/components/room.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+
+import React, { ReactNode } from "react"
+import { ClientSideSuspense  } from  "@liveblocks/react"
+
+import {RoomProvider} from "@/liveblocks.config"
+
+type RoomProps = {
+    children :ReactNode,
+    roomId: string
+}
+
+export const Room = ({children , roomId} :RoomProps ) => {
+    
+    return(
+    <RoomProvider id={roomId} initialPresence={{}}  >
+           <ClientSideSuspense fallback ={<div>...Loading</div>} >
+              {()=>children}
+           </ClientSideSuspense>
+     </RoomProvider>
+    )
+}
