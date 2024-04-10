c3ca38fdb6da0a9036eece338fa673b03f03e375
PALLAB GHOSH <gpallab405@gmail.com>
2024-04-10 20:00:56 +0530

    Create a < Skeleton /> component and integrate with CnvasLoader

--- board/app/board/[boardId]/_components/canvas-loading.tsx
+++ board/app/board/[boardId]/_components/canvas-loading.tsx
@@ -1,5 +1,7 @@
 import { Loader } from 'lucide-react'
 import React from 'react'
+import { InfoSkeleton } from './info'
+ 
 
 
 
@@ -10,6 +12,7 @@ export const CanvasLoading = () => {
   return (
     <main className="h-full w-full  bg-neutral-100 relative touch-none flex items-center justify-center">
         <Loader className='h-6 w-6 text-muted-foreground animate-spin' /> 
+         <InfoSkeleton/>
     </main>
   )
 }
--- board/app/board/[boardId]/_components/info.tsx
+++ board/app/board/[boardId]/_components/info.tsx
@@ -1,14 +1,18 @@
+import { Skeleton } from "@/components/ui/skeleton"
 
 
 
 
 export const Info = () => {
   return (
-    <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md">
+    <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md w-64">
       TODO:  Info about the board 
-
     </div>
   )
 }
 
- 
+export const InfoSkeleton = ()=>{
+   return (
+    <Skeleton className=" w-[300px] absolute top-2 left-2 bg-neutral-400 rounded-md px-1.5 h-12 flex items-center shadow-md"/>
+   )
+}
