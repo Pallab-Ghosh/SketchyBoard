b8b4bd7cac3be804197331d59d98e0367c57fc80
PALLAB GHOSH <gpallab405@gmail.com>
2024-04-13 01:05:06 +0530

    Create the route that fetch the data of authorization check and get the data of board and create the session for the user and send the token for accessing

---
+++ board/app/api/liveblocks-auth/route.ts
@@ -0,0 +1,61 @@
+import {auth , currentUser} from '@clerk/nextjs'
+import {Liveblocks} from "@liveblocks/node"
+import { ConvexClient, ConvexHttpClient} from "convex/browser"
+
+import { api } from "@/convex/_generated/api"
+
+const convex = new ConvexClient(process.env.NEXT_PUBLIC_CONVEX_URL!)
+
+const liveblocks = new Liveblocks({
+    secret : "sk_dev_f3lAJ_W4idapvNUm-g022k882tmVQ3Ff5Ux-UDYNQeLcSexuFofw90i2_UTTY9Fh"
+
+})
+
+export async function POST(request : Request){
+
+    const authorization = await auth();
+    const user  = await  currentUser()
+
+    console.log('AUTH INFO ', {
+        authorization,
+        user
+    })
+
+    if(!authorization || !user)
+        {
+            return new Response('Unauthorized' , {status : 403})
+        }
+    
+        const { room } = await request.json();
+        const board = await  convex.query(api.board.get , {id : room})
+
+         console.log('room board user auth config ' , {
+            room ,
+            board,
+            boardOrgId : board?.orgId,
+            userOrgId : authorization.orgId
+         })
+
+        if(board?.orgId !== authorization.orgId)
+            {
+                return new Response ("Unauthorized");
+            }
+
+        const userInfo = {
+            name : user.firstName || "Teammate",
+            picture:  user.imageUrl
+        }
+
+        console.log("user info ",userInfo)
+
+        const session = liveblocks.prepareSession(  user.id, {userInfo} )
+
+
+     if(room)
+        {
+            session.allow(room , session.FULL_ACCESS);
+        }
+        const { status, body } = await session.authorize();
+        console.log("ALLOWED" , {status , body})
+        return new Response(body, { status });
+}
--- board/liveblocks.config.ts
+++ board/liveblocks.config.ts
@@ -2,62 +2,17 @@ import { createClient } from "@liveblocks/client";
 import { createRoomContext, createLiveblocksContext } from "@liveblocks/react";
   
 const client = createClient({
-   publicApiKey: "pk_dev_ilqdonhgQCnFh7PAtFSi3g3wNhL-SP4SmuGPajpisbJcbGyHODsQ19mqlh-rC31e",
+   
+authEndpoint : "/api/liveblocks-auth",
+})
+ 
 
-  async resolveUsers({ userIds }) {
-    // Used only for Comments and Notifications. Return a list of user information
-    // retrieved from `userIds`. This info is used in comments, mentions etc.
-    
-    // const usersData = await __fetchUsersFromDB__(userIds);
-    // 
-    // return usersData.map((userData) => ({
-    //   name: userData.name,
-    //   avatar: userData.avatar.src,
-    // }));
-    
-    return [];
-  },
-  async resolveMentionSuggestions({ text }) {
-    // Used only for Comments. Return a list of userIds that match `text`.
-    // These userIds are used to create a mention list when typing in the
-    // composer. 
-    //
-    // For example when you type "@jo", `text` will be `"jo"`, and 
-    // you should to return an array with John and Joanna's userIds:
-    // ["john@example.com", "joanna@example.com"]
-
-    // const users = await getUsers({ search: text });
-    // return users.map((user) => user.id);
-
-    return [];
-  },
-  async resolveRoomsInfo({ roomIds }) {
-    // Used only for Comments and Notifications. Return a list of room information
-    // retrieved from `roomIds`.
-    
-    // const roomsData = await __fetchRoomsFromDB__(roomIds);
-    // 
-    // return roomsData.map((roomData) => ({
-    //   name: roomData.name,
-    //   url: roomData.url,
-    // }));
-    
-    return [];
-  },
-});
-
-// Presence represents the properties that exist on every user in the Room
-// and that will automatically be kept in sync. Accessible through the
-// `user.presence` property. Must be JSON-serializable.
 type Presence = {
   // cursor: { x: number, y: number } | null,
   // ...
 };
 
-// Optionally, Storage represents the shared document that persists in the
-// Room, even after all users leave. Fields under Storage typically are
-// LiveList, LiveMap, LiveObject instances, for which updates are
-// automatically persisted and synced to all connected clients.
+
 type Storage = {
   // author: LiveObject<{ firstName: string, lastName: string }>,
   // ...
