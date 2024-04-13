d20c9e9f1b7d1a257fc5e6c906b17ebc801cddd8
PALLAB GHOSH <gpallab405@gmail.com>
2024-04-13 19:18:04 +0530

    Create the dynamic page , fetch the data of board and render it on client , and also added a icon and button when click on that redirected to home page

--- board/app/board/[boardId]/_components/info.tsx
+++ board/app/board/[boardId]/_components/info.tsx
@@ -1,18 +1,68 @@
+'use client'
+
+import { Hint } from "@/components/hint"
+import { Button } from "@/components/ui/button"
 import { Skeleton } from "@/components/ui/skeleton"
+import { api } from "@/convex/_generated/api"
+import { Id } from "@/convex/_generated/dataModel"
+import { cn } from "@/lib/utils"
+ 
+import { useQuery } from "convex/react"
+import Image from "next/image"
+import Link from "next/link"
 
 
+type InfoProps = {
+  boardId : string
+}
+
+export const Info = ({boardId}:InfoProps) => {
+
+  const data = useQuery(api.board.get , {
+    id : boardId as Id <"boards">
+  })
+
+  if(!data)
+    {
+      return <InfoSkeleton/>
+    }
 
 
-export const Info = () => {
   return (
-    <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md w-64">
-      TODO:  Info about the board 
+
+    <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md">
+          <Hint label="Go to Boards Page" side="bottom" sideOffset={10}>
+            <Button asChild className=" px-2" variant="board">
+                  <Link href="/">
+                      <Image src="/logo.svg" alt="Board Logo" height={40} width={40} />
+                      <span className={cn("font-semibold text-xl ml-2 text-black")}>
+                          Board
+                      </span>
+                  </Link>
+              </Button> 
+          </Hint>
+        <Separator/>
+        <Button variant="board" className=" text-base font-normal" >{data.title}</Button>
     </div>
   )
 }
 
-export const InfoSkeleton = ()=>{
+
+
+
+export const InfoSkeleton =()=>{
    return (
-    <Skeleton className=" w-[300px] absolute top-2 left-2 bg-neutral-400 rounded-md px-1.5 h-12 flex items-center shadow-md"/>
+    <Skeleton className=" w-[300px] absolute top-2 left-2 bg-neutral-300 rounded-md px-1.5 h-12 flex items-center shadow-md"/>
    )
+}
+
+
+
+
+const Separator = ()=>{
+  return (
+    <div className=" text-neutral-300 px-1.5">
+
+    </div>
+  )
 }
