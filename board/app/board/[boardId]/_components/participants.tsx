99f094beb98cf65cfefe8774eb9c8bf244db6a7f
PALLAB GHOSH <gpallab405@gmail.com>
2024-04-15 20:14:16 +0530

    Fetch all users and current users , and send them to <Avatar/> component using map , and set a limit that except current user only 1 users shown in scrren and remaining will be seen as more

--- board/app/board/[boardId]/_components/participants.tsx
+++ board/app/board/[boardId]/_components/participants.tsx
@@ -1,11 +1,67 @@
- 
-import { Skeleton } from "@/components/ui/skeleton"
+'use client'
 
+import { Skeleton } from "@/components/ui/skeleton"
+import { useOthers, useSelf } from "@/liveblocks.config"
+import { UserAvatar } from "./user-avatar";
+ 
+ 
+
+const maximum_shown_users = 1;
 
 export const Participants = () => {
+
+  let users = useOthers();
+  const CurrentUser = useSelf()
+  const hasMoreUsers = users.length > maximum_shown_users
+
+ 
+ 
+ console.log('users',users)
+
   return (
-    <div className=" absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md w-[200px]">
-      List of users participants
+
+    <div className=" absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md ">
+          <div className=" flex gap-x-2">
+
+
+          {
+                CurrentUser && (
+                    <UserAvatar
+                      key={CurrentUser.connectionId}
+                      src={CurrentUser?.info.picture}
+                      name={CurrentUser?.info.name}
+                      fallback={CurrentUser?.info.name?.[0] || "T"}
+                      />
+                )
+              }
+
+
+              {
+                users.slice(0,maximum_shown_users).map((user_val)=>{
+                  return (
+                    <UserAvatar
+                      key={user_val.connectionId}
+                      src={user_val?.info.picture}
+                      name={user_val?.info.name}
+                      fallback={user_val?.info.name?.[0] || "T"}
+                    />
+                    
+                  )
+                })
+              }
+
+              {
+                hasMoreUsers &&  (
+                  <UserAvatar
+                    name={`${users.length  - maximum_shown_users} more`}
+                    fallback={ `+${users.length  - maximum_shown_users}`}
+                    />
+              )
+              }
+
+
+              
+          </div>
     </div>
   )
 }
