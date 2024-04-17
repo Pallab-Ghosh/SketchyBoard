5c42e2c910ad9918d1e8a9bc14cd52b94d1f3e0b
PALLAB GHOSH <gpallab405@gmail.com>
2024-04-18 01:17:59 +0530

    Get the property and functions from liveblock and send them as a props

--- board/app/board/[boardId]/_components/canvas.tsx
+++ board/app/board/[boardId]/_components/canvas.tsx
@@ -1,10 +1,15 @@
- 
+'use client'
 
+import { useState } from "react"
 import { Info } from "./info"
 import { Participants } from "./participants"
 import { Toolbar } from "./toolbar"
 
-import { useSelf } from "@/liveblocks.config"
+import { useCanRedo, useCanUndo, useHistory, useSelf } from "@/liveblocks.config"
+import { CanvasMode, CanvasState } from "@/types/canvas"
+
+
+ 
  
 type CanvasProps = {
   boardId : string
@@ -13,13 +18,27 @@ type CanvasProps = {
 
 const Canvas = ({boardId}:CanvasProps) => {
  
- 
+ const [canvasState , setCanvasState]= useState<CanvasState>({mode:CanvasMode.None})
 
+  const history = useHistory();
+  const canUndo = useCanUndo();//boolean value whether it is active or not
+  const canRedo = useCanRedo();//boolean value whether it is active or not
+
+
+
+ 
   return (
     <main className="h-full w-full  bg-slate-200 relative touch-none">
         <Info boardId={boardId}  />
         <Participants/>
-        <Toolbar/>
+        <Toolbar
+         canvasState={canvasState}
+         setCanvasState={setCanvasState}
+         undo ={history.undo}//functions for undo
+         redo = {history.redo}//functions for redo
+         canRedo={canRedo}
+         canUndo={canUndo}
+         />
     </main>
   )
 }
