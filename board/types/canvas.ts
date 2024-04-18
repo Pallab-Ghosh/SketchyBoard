909257794fca266f00b9786e2eb720211d85a67a
PALLAB GHOSH <gpallab405@gmail.com>
2024-04-18 16:55:01 +0530

    Create some more type and enum with specific value for specify the state of canvas

--- board/types/canvas.ts
+++ board/types/canvas.ts
@@ -1,9 +1,137 @@
-export type CanvasState = 
-  {
-     mode : CanvasMode.None | CanvasMode.Inserting |CanvasMode.Pencil |
-      CanvasMode.SelectionNet | CanvasMode.Translating | CanvasMode.Pressing | CanvasMode.Resizing
 
+export type Color = {
+   r: number,
+   g: number,
+   b:number
   }
+
+export type Camera = { 
+    x: number,
+    y: number
+  }
+
+export enum LayerType {
+  Rectangle ,
+  Ellipse ,
+  Path ,
+  Text ,
+  Note
+};
+
+
+export type RectangleLayer = {
+   type : LayerType.Rectangle ,
+   x : number ,
+   y : number ,
+   height : number ,
+   width : number ,
+   fill : Color,
+   value ?: string
+}
+
+export type EllipseLayer = {
+  type : LayerType.Ellipse ,
+  x : number ,
+  y : number ,
+  height : number ,
+  width : number ,
+  fill : Color,
+  value ?: string
+}
+
+export type PathLayer = {
+  type : LayerType.Path ,
+  x : number ,
+  y : number ,
+  height : number ,
+  width : number ,
+  fill : Color,
+  points: number[][],
+  value ?: string
+}
+
+export type TextLayer = {
+  type : LayerType.Text ,
+  x : number ,
+  y : number ,
+  height : number ,
+  width : number ,
+  fill : Color,
+  value ?: string
+}
+
+
+export type NoteLayer = {
+  type : LayerType.Note ,
+  x : number ,
+  y : number ,
+  height : number ,
+  width : number ,
+  fill : Color,
+  value ?: string
+}
+
+export type Point = { 
+  x: number,
+  y: number
+}
+
+
+export type XYWH = { 
+  x: number,
+  y: number,
+  height : number ,
+  width : number ,
+}
+
+export enum Side {
+   Top = 1 ,
+   Bottom = 2,
+   Left = 4 ,
+   Right = 8
+}
+
+
+export type CanvasState = 
+    {
+     mode : CanvasMode.None 
+
+    }
+
+  | {
+    mode : CanvasMode.Inserting,
+    layertype : LayerType.Text | LayerType.Ellipse | LayerType.Rectangle | LayerType.Note
+
+    }
+
+  | {
+    mode :CanvasMode.Pencil 
+    }
+
+  | {
+    mode: CanvasMode.Pressing,
+    origin :Point
+    }
+
+  | {
+      mode : CanvasMode.Translating
+      current: Point 
+    }
+
+  | {
+        mode : CanvasMode.Resizing,
+        initialBounds : XYWH,
+        corner: Side
+    }
+
+  | {
+      mode : CanvasMode.SelectionNet
+      origin: Point,
+      current?: Point
+    }
+
+
+
 
 
 export enum CanvasMode {
