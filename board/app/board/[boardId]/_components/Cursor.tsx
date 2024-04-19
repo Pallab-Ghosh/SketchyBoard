c4504986d50e3b4e233cffc38ad7defeed7eab65
PALLAB GHOSH <gpallab405@gmail.com>
2024-04-19 21:23:09 +0530

    Integrate   <foreignObject/> to render the html in svg tag

--- board/app/board/[boardId]/_components/Cursor.tsx
+++ board/app/board/[boardId]/_components/Cursor.tsx
@@ -1,26 +1,46 @@
- 'use client'
+'use client'
 
 import { useOther } from "@/liveblocks.config"
-import { memo } from "react"
+import { MousePointer2 } from "lucide-react"
+import { memo, useEffect } from "react"
 
  type CursorProps = {
     connectionId : number
  }
 
  export const Cursor = memo(({connectionId}:CursorProps)=>{
-    
+
+    //console.log('Cursor called')
+ 
+
     const info = useOther(connectionId , (user)=> user?.info)
     const cursor = useOther(connectionId , (user) => user.presence.cursor )
     
     const name = info?.name || "Teammate"
+    //console.log('info' ,  info ,'cursors', cursor)
 
     if(!cursor)
-        return null;
+        {
+            return null;
+        }
+   
 
     const {x, y} = cursor
+ 
 
     return(
-        <p></p>
+          <foreignObject
+            style={{transform: `translate(${x}px) translate(${y}px)` }}
+            height ={50}
+            width ={50}
+            className=" relative drop-shadow-md"
+          >
+             <MousePointer2 
+              className="h-5 w-5"
+              style={{fill:'beige' , color:'red'}}
+
+             />
+          </foreignObject>
     )
  })
  
