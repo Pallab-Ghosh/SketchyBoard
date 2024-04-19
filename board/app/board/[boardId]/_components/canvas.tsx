385e085ad43975114ce3cbb089233cb1d608acf4
PALLAB GHOSH <gpallab405@gmail.com>
2024-04-19 21:24:21 +0530

    Set the Camera state and create a function onPointerMove when mouse pointer move around the screen

--- board/app/board/[boardId]/_components/canvas.tsx
+++ board/app/board/[boardId]/_components/canvas.tsx
@@ -1,12 +1,12 @@
 'use client'
 
-import { useState } from "react"
+import React, { useState } from "react"
 import { Info } from "./info"
 import { Participants } from "./participants"
 import { Toolbar } from "./toolbar"
 
-import { useCanRedo, useCanUndo, useHistory, useSelf } from "@/liveblocks.config"
-import { CanvasMode, CanvasState } from "@/types/canvas"
+import { useCanRedo, useCanUndo, useHistory, useSelf , useMutation} from "@/liveblocks.config"
+import { Camera, CanvasMode, CanvasState } from "@/types/canvas"
 import { CursorPresence } from "./cursor-presence"
 
 
@@ -20,16 +20,29 @@ type CanvasProps = {
 const Canvas = ({boardId}:CanvasProps) => {
  
  const [canvasState , setCanvasState]= useState<CanvasState>({mode:CanvasMode.None})
+ const [camera , setCamera] = useState<Camera>({x:0 , y:0})
+
+
 
   const history = useHistory();
   const canUndo = useCanUndo();//boolean value whether it is active or not
   const canRedo = useCanRedo();//boolean value whether it is active or not
+ 
+ 
+  const onPointerMove = useMutation(({setMyPresence} , e:React.PointerEvent)=>{
+     
+    e.preventDefault();
 
+    const current = {  x:0 ,  y:0  }
+
+    setMyPresence({cursor:current})
+  },[])
 
 
  
   return (
     <main className="h-full w-full  bg-slate-200 relative touch-none">
+      
         <Info boardId={boardId}  />
         <Participants/>
         <Toolbar
@@ -40,6 +53,7 @@ const Canvas = ({boardId}:CanvasProps) => {
          canRedo={canRedo}
          canUndo={canUndo}
          />
+         
          <svg className="h-[100vh] w-[100vw]">
             <g>
                 <CursorPresence/>
