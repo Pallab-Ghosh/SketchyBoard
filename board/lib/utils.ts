af5c049f7b1fc12227c5222ed26e3a3b743d3e41
PALLAB GHOSH <gpallab405@gmail.com>
2024-04-23 02:03:48 +0530

    Create a ne wfunctions that calculate the canvas position and exclude the camera position

--- board/lib/utils.ts
+++ board/lib/utils.ts
@@ -2,7 +2,9 @@
 
 
 
+import { Camera } from "@/types/canvas"
 import { type ClassValue, clsx } from "clsx"
+import React from "react"
 import { twMerge } from "tailwind-merge"
 
 
@@ -20,4 +22,12 @@ export function cn(...inputs: ClassValue[]) {
 
 export function ConnectionIdToColor(connectionId:number) : string {
   return COLORS[connectionId % COLORS.length]
+}
+
+export function pointerEventToCanvasPoint(e:React.PointerEvent , camera :Camera )
+{
+    return {
+      x:Math.round(e.clientX) - camera.x,
+      y: Math.round(e.clientY) - camera.y
+    }
 }
