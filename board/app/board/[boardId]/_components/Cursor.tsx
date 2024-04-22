c22e6c63c623ec9ead277bec21b557d579132a80
PALLAB GHOSH <gpallab405@gmail.com>
2024-04-22 23:32:08 +0530

    Send the connectionId for uniqued color

--- board/app/board/[boardId]/_components/Cursor.tsx
+++ board/app/board/[boardId]/_components/Cursor.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { ConnectionIdToColor } from "@/lib/utils"
 import { useOther } from "@/liveblocks.config"
 import { MousePointer2 } from "lucide-react"
 import { memo, useEffect } from "react"
@@ -17,7 +18,7 @@ import { memo, useEffect } from "react"
     const cursor = useOther(connectionId , (user) => user.presence.cursor )
     
     const name = info?.name || "Teammate"
-    //console.log('info' ,  info ,'cursors', cursor)
+   
 
     if(!cursor)
         {
@@ -27,17 +28,20 @@ import { memo, useEffect } from "react"
 
     const {x, y} = cursor
  
-
+  
     return(
           <foreignObject
-            style={{transform: `translate(${x}px) translate(${y}px)` }}
+            style={{transform: `translateX(${x}px) translateY(${y}px)` }}
             height ={50}
             width ={50}
             className=" relative drop-shadow-md"
           >
              <MousePointer2 
               className="h-5 w-5"
-              style={{fill:'beige' , color:'red'}}
+              style={{
+               fill: ConnectionIdToColor(connectionId) ,
+               color: ConnectionIdToColor(connectionId)
+            }}
 
              />
           </foreignObject>
