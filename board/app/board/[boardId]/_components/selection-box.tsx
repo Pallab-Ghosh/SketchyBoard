7a71eb12e0c80a37b4e94dc092e045e91ff592a8
PALLAB GHOSH <gpallab405@gmail.com>
2024-05-03 23:22:25 +0530

    Check that selection layer is not Path type

--- board/app/board/[boardId]/_components/selection-box.tsx
+++ board/app/board/[boardId]/_components/selection-box.tsx
@@ -1,18 +1,33 @@
 'use client'
 
+import { LayerType, Side } from "@/types/canvas"
 import { memo } from "react"
+import { XYWH } from '../../../../types/canvas';
+import { useSelf, useStorage } from "@/liveblocks.config";
 
 
 type SelectionBoxProps ={
-    onResizeHandlePointerDown:()=>void
+    onResizeHandlePointerDown:(corner:Side ,initialBounds:XYWH)=>void
 }
 
+const handle_width = 8
 
 export const SelectionBox = memo(({onResizeHandlePointerDown}:SelectionBoxProps) => {
   
-    return (
+const soleLayerId = useSelf((me)=>me.presence.selection.length === 1 ? me.presence.selection[0] : null)
+
+
+ const isShowingandles = useStorage((root)=>
+ soleLayerId && root.layers.get(soleLayerId)?.type !==LayerType.Path
+)
+
+
+
+
+return (
+   
     <div>
-        
+
     </div>
   )
 })
